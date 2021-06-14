a264eb703d27d0e90e257250e3563e35bb51db8d
Harry Finn <harry@harryfinn.co.uk>
2021-06-14 16:56:25 +0100

    Initial migration from WP theme project to standalone package

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # TJ-Digital: Instagram
+
+## Requirements
+
+In order for the Instagram authentication process to work, you must setup a Facebook App with support for basic instagram display permissions. Once generated, add your `INSTAGRAM_CLIENT_ID` & `INSTAGRAM_CLIENT_SECRET` values to your project's `.env` file (or ENV vars depending on your setup) in order to succesfully complete authentication.
+
+## Setup
+
+Must be used with ACF and is design to work with an existing custom admin settings/options page to be setup similar, using the [`NanoSoup\Nemesis\ACF\BaseFields`](https://github.com/NanoSoup/Nemesis) package as per the example below.
+
+Note: Ensure you replace the `acf/render_field/name=` with the matching field value for your instagram field if different than `instagram_url`. The field has no bearing on the instagram account connected to the feed, this is handled during the instagram app authentication process.
+
+```php
+// Add in your SiteSettings class (or similar)
+use TJDigital\Instagram\InstagramSettings;
+
+// Configure the instagram field reference
+$prefix = 'site_settings';
+$settings_fields = [
+    $this->addTab($prefix, 'Social links'),
+    $this->url($prefix, 'Instagram URL'),
+];
+
+acf_add_local_field_group([
+    'key' => 'group_site_settings',
+    'title' => 'Site Settings',
+    'fields' => $settings_fields,
+    // etc
+]);
+
+// Add in your SiteSettings class (or similar) admin page constructor
+add_action('acf/render_field/name=instagram_url', [InstagramSettings::class, 'instagramAuthLink']);
+```
---
+++ src/admin/InstagramAuth.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace TJDigital\Instagram;
+
+/**
+ * Class InstagramAuth
+ */
+class InstagramAuth
+{
+    /**
+     * Redirect url used in other static methods
+     *
+     * @var string
+     */
+    private static $redirectUrl = '';
+
+    /**
+     * Builds the initial instagram auth url requiring user signon and authorisation
+     *
+     * @return string
+     */
+    public static function generateAuthorizeUrl(): string
+    {
+        $client_id = INSTAGRAM_CLIENT_ID;
+        $redirect_url = self::getRedirectUrl();
+        $base_url = 'https://www.instagram.com/oauth/authorize/';
+        $scopes = 'user_media,user_profile';
+
+        return "$base_url?client_id=$client_id&redirect_uri=$redirect_url&response_type=code&scope=$scopes";
+    }
+
+    /**
+     * Takes the initial auth code from Instagram FB App and generates a short live
+     *
+     * @param string $code Value returned form initial auth with Instagram app which can be converted into a short life
+     *               token.
+     *
+     * @return mixed
+     */
+    public static function requestShortLifeToken(string $code)
+    {
+
+        $response = wp_remote_post(
+            'https://api.instagram.com/oauth/access_token',
+            [
+                'method' => 'POST',
+                'body' => [
+                    'client_id' => INSTAGRAM_CLIENT_ID,
+                    'client_secret' => INSTAGRAM_CLIENT_SECRET,
+                    'grant_type' => 'authorization_code',
+                    'code' => $code,
+                    'redirect_uri' => self::getRedirectUrl(),
+                ]
+            ]
+        );
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+
+        if (is_wp_error($response) || isset($data['code']) && $data['code'] === 400) {
+            return [
+                'status' => 'error',
+                'error' => 'There was a problem exchanging tokens with Instagram, please try again later.'
+            ];
+
+            wp_die();
+        }
+
+        return ['status' => 'success', 'data' => (object) $data];
+
+        wp_die();
+    }
+
+    /**
+     * Exchanges a short life token for a long life token
+     *
+     * @param string $short_life_token A short life token returned by the initial Instagram app auth flow.
+     *
+     * @return mixed
+     */
+    public static function exchangeLongLifeToken(string $short_life_token)
+    {
+        $base_url = 'https://graph.instagram.com/access_token';
+        $client_secret = INSTAGRAM_CLIENT_SECRET;
+        $url = "$base_url?grant_type=ig_exchange_token&client_secret=$client_secret&access_token=$short_life_token";
+        $response = wp_remote_get($url);
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+
+        if (is_wp_error($response) || isset($data['error'])) {
+            return [
+                'status' => 'error',
+                'error' => 'There was a problem exchanging tokens with Instagram, please try again later.'
+            ];
+
+            wp_die();
+        }
+
+        return ['status' => 'success', 'data' => (object) $data];
+
+        wp_die();
+    }
+
+    /**
+     * Generates a new access token by refreshing an existing access token
+     *
+     * @param string $access_token An existing Instagram access token.
+     *
+     * @return mixed
+     */
+    public static function exchangeRefreshToken(string $access_token)
+    {
+        $base_url = 'https://graph.instagram.com/refresh_access_token';
+        $url = "$base_url?grant_type=ig_refresh_token&access_token=$access_token";
+        $response = wp_remote_get($url);
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+
+        if (is_wp_error($response) || isset($data['error'])) {
+            return [
+                'status' => 'error',
+                'error' => 'There was a problem refreshing tokens with Instagram, please try again later.'
+            ];
+
+            wp_die();
+        }
+
+        return ['status' => 'success', 'data' => (object) $data];
+
+        wp_die();
+    }
+
+    /**
+     * Returns the redirect url from variable if already set, else sets and returns the correct url
+     *
+     * @return string
+     */
+    private static function getRedirectUrl(): string
+    {
+        if (!empty(self::$redirectUrl)) {
+            return self::$redirectUrl;
+        }
+
+        self::$redirectUrl = trailingslashit(get_home_url()) . 'auth/instagram/callback';
+
+        return self::$redirectUrl;
+    }
+}
